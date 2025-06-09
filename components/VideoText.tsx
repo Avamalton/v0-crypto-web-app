3a4552063880800a40111473ad68639c940046ba
Avamalton <avamalton9@gmail.com>
2025-06-10 05:38:57 +0700

    video

--- components/VideoText.tsx
+++ components/VideoText.tsx
@@ -2,7 +2,7 @@ import { VideoText } from "@/components/magicui/video-text";
 
 export function VideoTextDemo() {
   return (
-<div className="absolute h-[300px] w-[600px] mx-auto overflow-hidden flex items-center justify-center">
+    <div className="absolute w-full max-w-[600px] h-[200px] sm:h-[250px] md:h-[300px] mx-auto overflow-hidden flex items-center justify-center px-4">
       <VideoText src="/particle.mp4">
         CONNECT
       </VideoText>
